d42825b0fd5e91409921257ec8aa7be14dfaad7f
Clinton Halpin <clintonhalpin@gmail.com>
2016-12-19 21:20:59 -0500

    Add GA

--- components/Root.jsx
+++ components/Root.jsx
@@ -13,9 +13,12 @@ var Root = React.createClass({
   },
 
   render: function () {
+
     var initialProps = {
       __html: safeStringify(this.props)
     }
+
+    var script = { __html : "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', 'UA-89276109-1', 'auto');ga('send', 'pageview');" };
 
     return (
       <html>
@@ -33,6 +36,7 @@ var Root = React.createClass({
             type='application/json'
             dangerouslySetInnerHTML={initialProps} />
           <script src='/bundle.js' />
+          <script dangerouslySetInnerHTML={script} />
         </body>
       </html>
     )
